881cbc9b99e1104172daaf253ee5220881384358
LahiruNr <139838828+LahiruNr@users.noreply.github.com>
2023-07-18 10:53:04 +0530

    Create index.js

---
+++ index.js
@@ -0,0 +1,13 @@
+// JavaScript function to change the text when the button is clicked
+function changeText(){
+    var textArray = ["Text1", "Text2" , "Text3", "Text4", "Text5"]
+    var number = getRandomNumberBetween(0, textArray.length - 1)
+    console.log("Index: ", number)
+    document.getElementById("heading").innerHTML = textArray[number];
+}
+
+function getRandomNumberBetween(min, max){
+    return Math.floor(Math.random() * (max-min+1)+min);
+}
+
+
